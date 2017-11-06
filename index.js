b6b088baffd0dafdd8e0a0d095a6e3cc6ae3de34
Ryan Do <falcsmike7@gmail.com>
2017-11-06 21:42:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    let endDate = new Date(year, 0, 1);
+    return Math.round((endDate - this.startDate) / (365*24*60*60*1000) );
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    
+  }
+}
